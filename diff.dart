cf7303ac9e528bd475e4063658e2f986cb89d59e
John Colagioia <john@colagioia.net>
2018-01-29 06:24:48 -0500

    Track start and end of similar sections

--- diff.dart
+++ diff.dart
@@ -70,6 +70,11 @@ List<String> longestCommonSubstring(List<String> first, List<String> second) {
   List<List<int>> num = new List<List<int>>(first.length);
   int maxlen = 0;
   int lastSubsBegin = 0;
+  int aFirst = -1;
+  int zFirst = -1;
+  int aSecond = -1;
+  int zSecond = -1;
+  
   for (int i = 0; i < first.length; i++) {
     num[i] = new List<int>(second.length);
     for (int j = 0; j < second.length; j++) {
@@ -86,11 +91,22 @@ List<String> longestCommonSubstring(List<String> first, List<String> second) {
           maxlen = num[i][j];
           int thisSubsBegin = i - num[i][j] + 1;
           if (lastSubsBegin == thisSubsBegin) {
+            if (aFirst < 0) {
+              aFirst = i;
+              aSecond = j;
+            }
+            zFirst = i;
+            zSecond = j;
             sequence.add(first[i]);
           } else {
             lastSubsBegin = thisSubsBegin;
+            int nWords = i + 1 - lastSubsBegin;
             sequence = new List<String>();
-            for (int k = 0; k < i + 1 - lastSubsBegin; k++) {
+            aFirst = i;
+            aSecond = j;
+            zFirst = i + nWords;
+            zSecond = j + nWords;
+            for (int k = 0; k < nWords; k++) {
               sequence.add(first[lastSubsBegin + k]);
             }
           }
